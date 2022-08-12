e6926b0261773256cf3102e5de7e1fc59b896101
imagi <9677226@mail.ru>
2022-08-12 15:17:45 +0600

    Added SMS code entry

--- src/main/java/kz/munda/AuthBuyerPage.java
+++ src/main/java/kz/munda/AuthBuyerPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
-
+import kz.munda.WebDriverSettings;
 import static kz.munda.variable.*;
 import static org.testng.Assert.assertEquals;
 
--- src/main/java/kz/munda/RegBuyerTest.java
+++ src/main/java/kz/munda/RegBuyerTest.java
@@ -2,61 +2,79 @@ package kz.munda;
 
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import static kz.munda.variable.*;
 
 public class RegBuyerTest extends WebDriverSettings{
 
     TelegramSend telegram = PageFactory.initElements(driver, TelegramSend.class);
 
+    function func = PageFactory.initElements(driver, function.class);
+
+
     @Test(description = "Регистрация покупателя", groups = "munda_organization")
     public void regBuyer() throws Exception {
 
-        String testcase = "1.1 Регистрация покупателя (Ожидаемое поведение системы)";
+       // String testcase = "1.1 Регистрация покупателя (Ожидаемое поведение системы)";
         RegBuyerPage bayerPage = PageFactory.initElements(driver, RegBuyerPage.class);
         //deleteAllFilesFolder();
-        telegram.getpost("Тест кейс №"+ testcase +" Тестирование начато");
+        //telegram.getpost("Тест кейс №"+ testcase +" Тестирование начато");
+        String randomphone = phone;
 
         bayerPage.open(registrationUrl);
-        bayerPage.insertPhone(phone);
+        bayerPage.insertPhone(randomphone);
         bayerPage.clickBtn();
-        bayerPage.sleep(2000);
+        func.sleep(2000);
+
+        String code = telegram.getCode(randomphone);
+        System.out.println("Code iz sms:" + code);
+        bayerPage.insertSmsCode(code);
+        func.sleep(2000);
+        bayerPage.smsBtnClick();
+
+        func.sleep(2000);
         bayerPage.insertName(name);
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertSurName(surname);
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertEmail(email);
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.clickIagree();
-        bayerPage.sleep(1000);
-        bayerPage.screenShots(testcase);
+        func.sleep(1000);
+       // bayerPage.screenShots(testcase);
         bayerPage.clickBtnNext();
 
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.clickCountry();
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.clickCountryElement();
 
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.clickCity();
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.clickCityElement();
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertAdress(adress);
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertPostIndex(postindex);
         bayerPage.clickBtnNext2();
-        bayerPage.screenShots(testcase);
+        //bayerPage.screenShots(testcase);
 
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertPassword(password);
-        bayerPage.sleep(1000);
+        func.sleep(1000);
         bayerPage.insertPassword2(password);
-        bayerPage.screenShots(testcase);
-        bayerPage.sleep(1000);
+        //bayerPage.screenShots(testcase);
+        func.sleep(1000);
         bayerPage.clickBtnNext3();
-        bayerPage.sleep(3000);
+        func.sleep(3000);
         bayerPage.checkCurrentUrl(baseUrl);
-        telegram.getpost("Тест кейс №"+ testcase +" Тестирование успешно завершено");
+       // telegram.getpost("Тест кейс №"+ testcase +" Тестирование успешно завершено");
 
     }
 
@@ -73,12 +91,19 @@ public class RegBuyerTest extends WebDriverSettings{
         bayerPage.open(registrationUrl);
         bayerPage.insertPhone(falsePhone);
         bayerPage.clickBtn();
-        bayerPage.sleep(3000);
+        func.sleep(3000);
         telegram.getpost("Тест кейс №"+ testcase +" Тестирование успешно завершено");
         bayerPage.screenShots(testcase);
-        bayerPage.sleep(3000);
+        func.sleep(3000);
         bayerPage.checkCurrentUrl(baseUrl + registrationUrl);
 
     }
 
+
+    @Test()
+    public void testSms() throws Exception {
+
+        telegram.getCode("77029677226");
+
+    }
 }
